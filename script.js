00a8c05555331814d2de70e61538dbc2c311b711
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-02-02 09:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function onesDigit(n) {
   // your code here
-	return n[n.length-1]
+n = n.toString();        // convert number to string safely
+  return Number(n[n.length - 1]);
 }
 
 // Do not change the code below
 
-const n = prompt("Enter Number:");
+//const n = prompt("Enter Number:");
 alert(onesDigit(n));
